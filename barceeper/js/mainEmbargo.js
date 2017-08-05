d0923591363c82f87155822eb1b6f4b15a874a30
Denis Fedioukovitch <denis3901442@gmail.com>
2017-08-05 17:46:07 +0300

    v 0.0.2.1

--- barceeper/js/mainEmbargo.js
+++ barceeper/js/mainEmbargo.js
@@ -1,1 +1,18 @@
 
+function startWork() {
+  if(event.target.getAttribute('data-status') != "active") return;
+
+  document.querySelector('.stopButton').classList.toggle('close');
+  event.target.setAttribute('data-status','unactive');
+}
+
+document.querySelector('.startButton').onclick = startWork;
+
+
+function stopWork() {
+  event.target.classList.toggle('close');
+  document.querySelector('.startButton').setAttribute('data-status', 'active');
+}
+
+
+document.querySelector('.stopButton').onclick = stopWork;
