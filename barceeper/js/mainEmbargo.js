8dd652073dab11da95875c924b25e5869a87d4bb
Denis Fedioukovitch <denis3901442@gmail.com>
2017-08-05 17:29:58 +0300

    v 0.0.2

--- barceeper/js/mainEmbargo.js
+++ barceeper/js/mainEmbargo.js
@@ -1,14 +1,1 @@
 
-let a=document.getElementById('start');
-console.log(a[0]);
-
-
-function startWork() {
-  if( document.getElementById('start')[0].style.background == "green" ) {
-    end.classList.toggle(hide);
-    start.style.background == "rgb(6,61,3)";
-  }
-}
-
-console.log(start.style.background)
-start.onclick=startWork;
