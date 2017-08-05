ce5927188fdfea6215ca7f44b74ccaeee7af0c02
Denis Fedioukovitch <denis3901442@gmail.com>
2017-08-05 19:16:37 +0300

    v 0.0.2.2

--- barceeper/js/mainEmbargo.js
+++ barceeper/js/mainEmbargo.js
@@ -4,15 +4,30 @@ function startWork() {
 
   document.querySelector('.stopButton').classList.toggle('close');
   event.target.setAttribute('data-status','unactive');
+  showStats();
 }
-
-document.querySelector('.startButton').onclick = startWork;
 
 
 function stopWork() {
   event.target.classList.toggle('close');
   document.querySelector('.startButton').setAttribute('data-status', 'active');
+
+  setTimeout(function(){
+    document.querySelector('.statButton').innerText = "stats";
+    document.querySelector('.statButton').classList.add('buttons');
+    document.querySelector('.statButton').classList.remove('stats');
+  }, 5000);
 }
 
 
+function showStats() {
+  document.querySelector('.statButton').classList.remove('buttons');
+  document.querySelector('.statButton').classList.add('stats');
+  document.querySelector('.statButton').style.width="300px";
+  document.querySelector('.statButton').style.height="300px";
+  document.querySelector('.statButton').innerText = "";
+}
+
+document.querySelector('.startButton').onclick = startWork;
 document.querySelector('.stopButton').onclick = stopWork;
+document.querySelector('.statButton').onclick = showStats;
