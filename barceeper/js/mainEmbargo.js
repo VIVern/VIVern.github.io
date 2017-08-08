bd1997f940c48c6753d668a02e8600cdfd2481e6
Denis Fedioukovitch <denis3901442@gmail.com>
2017-08-08 16:02:34 +0300

    v 0.0.3

--- barceeper/js/mainEmbargo.js
+++ barceeper/js/mainEmbargo.js
@@ -11,25 +11,34 @@ function startWork() {
 function stopWork() {
   event.target.classList.toggle('close');
   document.querySelector('.startButton').setAttribute('data-status', 'active');
-
-  setTimeout(function(){
-    document.querySelector('.statButton').innerText = "stats";
-    document.querySelector('.statButton').style.width="290px";
-    document.querySelector('.statButton').style.height="130px";
-    document.querySelector('.statButton').classList.toggle('buttons');
-    document.querySelector('.statButton').classList.toggle('stats');
-  }, 5000);
 }
 
 
 function showStats() {
+  if(event.target != document.querySelector('.statButton') && event.target != document.querySelector('.startButton')) return;
   document.querySelector('.statButton').classList.remove('buttons');
+  document.querySelector('.statButton').classList.remove('stButHover');
   document.querySelector('.statButton').classList.add('stats');
-  document.querySelector('.statButton').style.width="300px";
+  document.querySelector('.statButton').style.width="320px";
   document.querySelector('.statButton').style.height="300px";
-  document.querySelector('.statButton').innerText = "";
+  setTimeout(function(){
+    document.querySelector('.statContainer').classList.remove('hide');
+  }, 500)
 }
 
+function closeStats() {
+  document.querySelector('.statContainer').classList.add('hide');
+  document.querySelector('.statButton').style.width="290px";
+  document.querySelector('.statButton').style.height="130px";
+  document.querySelector('.statButton').classList.remove('stats');
+  document.querySelector('.statButton').classList.add('buttons');
+  setTimeout (function(){
+    document.querySelector('.statButton').classList.add('stButHover');
+  }, 1000);
+}
+
+
+document.querySelector('.closeStat').onclick = closeStats;
 document.querySelector('.startButton').onclick = startWork;
 document.querySelector('.stopButton').onclick = stopWork;
 document.querySelector('.statButton').onclick = showStats;
